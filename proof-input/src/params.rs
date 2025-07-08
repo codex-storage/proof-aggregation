27e081d7df7a7dd19a305ed757ade1b652a54af2
M Alghazwi <m.ghazwi@gmail.com>
2025-07-08 18:56:12 +0200

    remove H and C types

--- proof-input/src/params.rs
+++ proof-input/src/params.rs
@@ -1,21 +1,8 @@
 // params for generating input for proof circuit
-
-use plonky2::hash::poseidon::PoseidonHash;
-use plonky2::plonk::config::PoseidonGoldilocksConfig;
-use plonky2::plonk::config::GenericConfig;
 use std::env;
 use anyhow::{Result, Context};
 use codex_plonky2_circuits::circuits::params::CircuitParams;
-use plonky2_poseidon2::config::Poseidon2GoldilocksConfig;
-use plonky2_poseidon2::poseidon2_hash::poseidon2::Poseidon2Hash;
 
-// test types
-pub const D: usize = 2;
-pub type C = PoseidonGoldilocksConfig;
-pub type C2 = Poseidon2GoldilocksConfig;
-pub type F = <C as GenericConfig<D>>::F; // this is the goldilocks field
-pub type HF = PoseidonHash;
-pub type HF2 = Poseidon2Hash;
 
 // hardcoded default params for generating proof input
 const DEFAULT_MAX_DEPTH: usize = 32; // depth of big tree (slot tree depth, includes block tree depth)
